c826b02c33741835addb2f2f1e62ebc209147c86
nando-carvalho <fernando@elosistemas.inf.br>
2022-04-01 09:51:23 -0300

    Configurando botões do pomodoro; Working, Pause e Rest; Definindo e codando sons;

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
       <PomodoroTimer
         pomodoroTime={1500}
         shortRestTime={300}
-        LongRestTime={900}
+        longRestTime={900}
         cycles={4}
       />
     </div>
--- src/componentes/pomodoro-timer.tsx
+++ src/componentes/pomodoro-timer.tsx
@@ -1,29 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from '../componentes/button';
 import { Timer } from '../componentes/timer';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../sounds/src_sounds_bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../sounds/src_sounds_bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
 
 interface Props {
   pomodoroTime: number;
   shortRestTime: number;
-  LongRestTime: number;
+  longRestTime: number;
   cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
+  const [resting, setResting] = React.useState(false); //em repouso
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+    if (resting) document.body.classList.remove('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+    audioStartWorking.play();
+  };
+
+  const configureRest = (long: boolean) => {
+    setTimeCounting(true);
+    setWorking(false);
+    setResting(true);
+    if (long) {
+      setMainTime(props.longRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
+    audioStopWorking.play();
+  };
   return (
     <div className="pomodoro">
       <h2>You are: Working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
-        <Button text="Working" onClick={() => console.log(1)}></Button>
-        <Button text="Stop" onClick={() => console.log(1)}></Button>
-        <Button text="Go" onClick={() => console.log(1)}></Button>
+        <Button text="Work" onClick={() => configureWork()}></Button>
+        <Button text="Rest" onClick={() => configureRest(false)}></Button>
+        <Button
+          className={!working && !resting ? 'hidden' : ''}
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
       </div>
       <div className="details">
         <p>Teste</p>
